c4ddcc57f9ef7d4248828c7bf4b68c00ee6c0811
PHien <quocphientruong@gmail.com>
2019-02-08 22:30:35 +0700

    get quote random

--- src/web/fns/src/App.js
+++ src/web/fns/src/App.js
@@ -2,28 +2,28 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import firebase from './firebase';
-
 class App extends Component {
   constructor(props) {
     super(props);
+
     this.ref = firebase.firestore().collection('quotes');
     this.unsubscribe = null;
     this.state = {
-      quotes: []
-    };
+      quote: ''
+    };  
   }
 
   onCollectionUpdate = (querySnapshot) => {
-    const quotes = [];
-    querySnapshot.forEach((doc) => {
-      const { vi_value, author } = doc.data();
-      quotes.push({
-        vi_value,
-        author,
-      });
-    });
+    const numberOfQuote = querySnapshot.docs.length;
+    const randomIndex = Math.floor(Math.random() * numberOfQuote);
+    console.log(numberOfQuote);
+    console.log(randomIndex);
+
+    // get random quote
+    const quote = querySnapshot.docs[randomIndex].data();
+
     this.setState({
-      quotes
+      quote
    });
   }
 
@@ -34,17 +34,15 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        {this.state.quotes.map(quote =>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            {quote.vi_value}
+            {this.state.quote.vi_value}
           </p>
           <p className="App-link">
-            {quote.author}
+            {this.state.quote.author}
           </p>
         </header>
-        )}
       </div>
     );
   }
